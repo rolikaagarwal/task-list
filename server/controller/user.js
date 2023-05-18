feeec399e484e6f973e4479474e125d202efd55d
Rolika Agarwal <rolikaagarwal2003@gmail.com>
2023-05-18 06:41:35 +0530

    get user added

--- server/controller/user.js
+++ server/controller/user.js
@@ -9,3 +9,18 @@ exports.createUser = async (req, res) => {
     res.status(400).json(err);
   }
 };
+
+exports.getUsers = (req, res) => {
+  model.User.find({})
+  .then(allUsers => {
+    res.status(200).json(allUsers);
+  })
+  .catch(error => {
+    console.error(error);
+    res.status(400).json({
+      message: "Error in getting user",
+      error: error.message
+    });
+  });
+};
+
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require("../controller/user") 
 router
 .post("/", userController.createUser)
+.get("/",userController.getUsers)
 
 exports.router = router;
